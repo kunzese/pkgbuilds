b358c8370546dbd6ae2a7644e3eb4af0e9730d7a
Sebastian Kunze <mail@sebastiankunze.de>
2021-11-29 13:15:14 +0100

    ci(github-actions): add release-package workflow

---
+++ .github/scripts/release-package.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -eo pipefail
+
+PACKAGE=$1
+TEMPDIR="$(mktemp -d)"
+
+source "./$PACKAGE/PKGBUILD"
+
+git clone "ssh://aur@aur.archlinux.org/$1.git" "$TEMPDIR"
+cp ./"$PACKAGE"/{PKGBUILD,.SRCINFO} "$TEMPDIR"
+
+pushd "$TEMPDIR"
+
+if [ -n "$(git status --porcelain)" ]; then
+  # Uncommitted changes
+  git commit -a -m "chore($PACKAGE): release version $pkgver-$pkgrel"
+  git push
+else 
+  # Working directory clean
+  echo "No changes detected"
+fi
