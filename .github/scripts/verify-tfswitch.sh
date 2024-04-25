989d273457209e9562a34a5c665acd9b338d88b8
Sebastian Kunze <mail@sebastiankunze.de>
2024-04-25 12:01:24 +0200

    fix(tfswitch): add v-prefix in build step

--- .github/scripts/verify-tfswitch.sh
+++ .github/scripts/verify-tfswitch.sh
@@ -5,7 +5,7 @@ set -eo pipefail
 source ./PKGBUILD
 
 BINARY_VERSION=$(command tfswitch --version | tr -d '\n' | sed 's/^Version: //')
-if [ "$BINARY_VERSION" != "$pkgver" ]; then
+if [ "$BINARY_VERSION" != "v$pkgver" ]; then
   >&2 echo "tfswitch --version reported version $BINARY_VERSION instead of expected version $pkgver"
   exit 3
 else
