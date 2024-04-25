7636909120f54bb2ebb3afaecfb31f94ed7d5a11
Sebastian Kunze <mail@sebastiankunze.de>
2024-04-25 11:04:27 +0200

    ci: test packages on pull request

---
+++ .github/scripts/verify-tfswitch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -eo pipefail
+
+source ./PKGBUILD
+
+BINARY_VERSION=$(command tfswitch --version | tr -d '\n' | sed 's/^Version: //')
+if [ "$BINARY_VERSION" != "$pkgver" ]; then
+  >&2 echo "tfswitch --version reported version $BINARY_VERSION instead of expected version $pkgver"
+  exit 3
+else
+  echo "tfswitch --version reported version $BINARY_VERSION"
+fi
